b5704886ac44efc830407d1446132d7909312f65
Tanel Lebedev <tanel.lebedev@gmail.com>
2018-04-09 12:33:56 +0300

    Refactor

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 $(function () {
+
 	$("form").submit(function (e) {
 		e.preventDefault();
-		var choices = $('input[type="text"]');
-		var r = Math.floor(Math.random() * choices.length);
-		var input = $(choices[r]);
-		var span = input.next();
+
+		var choices = $('input[type="text"]'),
+			r = Math.floor(Math.random() * choices.length),
+			input = $(choices[r]),
+			span = input.next();
+
 		span.removeClass("hidden");
-		console.log(input);
-		console.log(r);
 	});
+
 });
