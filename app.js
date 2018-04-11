15c654d6a2c0c6580596d980f8b785912eed6c6c
Tanel Lebedev <tanel.lebedev@gmail.com>
2018-04-11 22:19:46 +0300

    Refactor UI

--- app.js
+++ app.js
@@ -1,14 +1,47 @@
 $(function () {
+	var result = $('.result'),
+		entry = $('.entry');
 
-	$("form").submit(function (e) {
+	var collectChoices = function () {
+		var inputs = entry.find('input[type="text"]'),
+			input,
+			i,
+			text,
+			choices = [];
+
+		for (i = 0; i < inputs.length; i++) {
+			input = $(inputs[i]);
+			text = input.val();
+			if (text !== '') {
+				choices.push(text);
+			}
+		}
+
+		return choices;
+	};
+
+	var calculateChoice = function () {
+		var choices = collectChoices(),
+			r = Math.floor(Math.random() * choices.length);
+
+		return choices[r];
+	};
+
+	var choose = function (e) {
 		e.preventDefault();
 
-		var choices = $('input[type="text"]'),
-			r = Math.floor(Math.random() * choices.length),
-			input = $(choices[r]),
-			span = input.next();
+		result.find('h1').text(calculateChoice());
+		entry.addClass('invisible');
+		result.removeClass('invisible');
+	};
 
-		span.removeClass("hidden");
-	});
+	var startEntry = function (e) {
+		e.preventDefault();
 
+		entry.removeClass('invisible');
+		result.addClass('invisible');
+	};
+
+	entry.find("form").submit(choose);
+	result.find("form").submit(startEntry);
 });
